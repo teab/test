5f17de7fa4e72b801b6876ef2738a6cec21a514f
ynjswjsw <ynjswjsw@sina.com>
2013-11-08 14:10:47 +0800

    html
    html

---
+++ js/common.js
@@ -0,0 +1,7 @@
+/*作者(Author): 一木梦林(Dream lam)    Website: www.dreamlam.com    E-mail: yimumenglin@163.com    QQ: 348599167*/
+$(document).ready(function() {
+	$("a").focus(function() {
+		$(this).blur();
+	});
+	//=======================hot zone_end=======================
+});
